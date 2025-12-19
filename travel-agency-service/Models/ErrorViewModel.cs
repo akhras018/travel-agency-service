ad579463da071c048ee7c16d62704778d2187722
akhras018 <akhras018@gmail.com>
2025-12-19 15:50:45 +0200

    Add project files.

---
+++ travel-agency-service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace travel_agency_service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
